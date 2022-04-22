d9b823e3f5a8d612e70ccceaf8da1b61eaf9914a
Derek <derekdanielson84@gmail.com>
2022-04-22 13:26:38 -0600

    Add files via upload

---
+++ javascriptNotes.js
@@ -0,0 +1,86 @@
+//COMMENTS - Single Line
+/* Multi
+    line */
+
+//DECLARE VARIABLE
+var thisVariable;
+
+//STORE VALUES WITH THE ASSIGNMENT OPERATOR
+var a;
+a = 10;
+
+//ASSIGN VALUE OF ONE VARIABLE TO ANOTHER
+var a;
+a = 7;
+var b;
+b = a;
+
+//INITIALIZING VARIABLES WITH THE ASSIGNMENT OPERATOR
+var c = 10;
+
+//CASE SENSITIVITY IN VARIABLES
+camelCase
+
+
+
+/* !!!!!!!!!!!!!!!! 
+Use uppercase for naming immutable values and lowercase or camelCase for mutable values (arrays, objects) 
+!!!!!!!!!!!!!! */
+
+
+
+//Math
+let sum = 3 + 10;
+let sum = 3 - 10;
+let sum = 3 * 10;
+let sum = 3 / 10;
+
+
+//INCREMENT A NUMBER
+let myVar = 83;
+myVar++;
+//Equivalent to myVar = myVar + 1;
+
+//DECREMENT A NUMBER
+let myVar = 83;
+myVar--;
+//Equivalent to myVar = myVar + 1;
+
+
+//FLOATING POINT/DECIMAL NUMBERS
+let myVar = 3.8;
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
